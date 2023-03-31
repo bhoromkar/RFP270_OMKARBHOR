253ff7c632fa0ad0aff2df83ca60f5ad99927ab1
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-03-31 16:01:18 +0530

    updated assignment

--- methods/EmployeeWage.java
+++ methods/EmployeeWage.java
@@ -1,7 +1,35 @@
 package com.bridgelabz.methods;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
+    static void employeewage() {
+        int days =1;
+        int count;
+        {
+            count = 1;
 
+            //while(days==30)
+            while (count<20){
+                double isPresent = (int) (Math.random() * 10 % 2);
+                int present = (int) isPresent;
+                   //days++;
+                if (present == 1) {
+                    count++;
+
+                    System.out.println("employee is present on day " + count);
+                } else {
+                    System.out.println("employee is absent");
+                }
+
+
+            }
+
+        }
+            int wage = 8 * 20 * count;
+            System.out.println(count);
+            System.out.println(wage);
+        }
+
+    public static void main(String[] args) {
+      employeewage();
     }
 }
